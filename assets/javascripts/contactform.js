1eae3bf3ecfebb914376a73e7aa4c2754599935b
Oyasi Kelly <104142584+Oyasikelly@users.noreply.github.com>
2025-03-03 11:44:11 +0100

    Made some adjustments

--- assets/javascripts/contactform.js
+++ assets/javascripts/contactform.js
@@ -1,28 +1,26 @@
 $(function () {
-    "use strict";
-    
-    /*  
-        Template name    : Technoit - IT Solutions & Business Services Multipurpose Responsive Website Template
+  "use strict";
+
+  /*  
+        Template name    : Boost Business Hub - IT Solutions & Business Services Multipurpose Responsive Website Template
         Author           : ZRTHEMES
         Version          : 1.0
         File Description : contact js file of the template
     */
-    $('#ajax-contact').validator();
-    $('#ajax-contact').on('submit', function (e) {
-        if (!e.isDefaultPrevented()) {
-            var url = "assets/phpscripts/contact.html";
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: $(this).serialize(),
-                success: function (data)
-                {
-                    $( "#msgSubmit" ).removeClass( "hidden" );
-                    $('#ajax-contact')[0].reset();
-                    
-                }
-            });
-            return false;
-        }
-    })
+  $("#ajax-contact").validator();
+  $("#ajax-contact").on("submit", function (e) {
+    if (!e.isDefaultPrevented()) {
+      var url = "assets/phpscripts/contact.html";
+      $.ajax({
+        type: "POST",
+        url: url,
+        data: $(this).serialize(),
+        success: function (data) {
+          $("#msgSubmit").removeClass("hidden");
+          $("#ajax-contact")[0].reset();
+        },
+      });
+      return false;
+    }
+  });
 });
